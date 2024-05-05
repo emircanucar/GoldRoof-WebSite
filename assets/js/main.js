485628edda7b4843692a004a87100a26c48c851e
emircanucar <133085075+emircanucar@users.noreply.github.com>
2024-05-05 10:28:34 +0300

    References section added and product images added  in files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,31 @@
 var swiper = new Swiper(".mainSlider", {});
+var swiper = new Swiper(".referenceSlider", {
+  slidesPerView: 1.2,
+  spaceBetween: 10,
 
+  breakpoints: {
+    576: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3.5,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4.5,
+      spaceBetween: 20,
+    },
+    1280: {
+      slidesPerView: 5.5,
+      spaceBetween: 20,
+    },
+    1536: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+  },
+});
 var endDate = new Date("jun 15, 2024 23:59").getTime();
 
 var x = setInterval(() => {
